7a7ffdcdf8b7bb656b00470cc986000dfa795003
Wiess <1053137796@qq.com>
2022-12-27 17:53:40 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,15 @@ const path = require('path')
 
 // 导出webpack的配置对象
 module.exports = {
+  mode : 'development',  // 开发环境
+  // mode : 'production',  // 生产环境
+
   // webpack的入口
   entry: './src/index.js',
+  devServer:{   // 开发服务器配置
+    open: true,  // 启动时默认打开浏览器
+    port: 8888  // 启动的端口号
+  },
   output: {
     // path 必须用绝对地址 : path.resolve()拼接2个路径
     // __dirname: node内置全局变量（值：当前文件所在文件夹的绝对路径）
@@ -16,8 +23,8 @@ module.exports = {
   plugins: [
     // 自动生成html
     new HtmlWebpackPlugin({
-      title: 'Wiess',
-      template: './public/index.html'
+      // title: 'WebPack',
+      template: './public/index.html',
     })],
   // 加载器
   module: {
@@ -50,15 +57,16 @@ module.exports = {
         generator: { 
           filename: 'font/[name].[hash:6][ext]' // 放到dist/font文件夹, 文件名格式如左
         }
-      },
+      }, 
       // babel处理高等级的js语法
       {
-        test: /\.m?js$/,
+        test: /\.m?js$/, // 匹配.mjs或者.js结尾文件
+        // 排除node_modules第三方包里js文件=>别人写的
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader', // 允许webpack使用babel降级js代码
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'] // 使用这个包里记录的规则
           }
         }
       }
